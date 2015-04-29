e0f524c0662aae370a5f531adaf1bd1a6c30fcae
Vladimir Varankin <nek.narqo+git@gmail.com>
2015-04-30 01:02:51 +0300

    refactoring ./run ./make

--- run.sh
+++ run.sh
@@ -1,13 +1,13 @@
 #!/bin/sh
 
-cd yate
-./run.sh 2> /dev/null
-cd ..
+run()
+{
+  cd $1
+  ./run.sh 2> /dev/null
+  cd ..
+}
 
-cd teya
-./run.sh 2> /dev/null
-cd ..
-
-cd bh
-./run.sh 2> /dev/null
-cd ..
+for engine in yate teya bh bemhtml; do
+  echo run $engine...
+  run $engine
+done
